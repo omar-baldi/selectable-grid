c2bf1b309b8ba82dbc8cf53713811816ab22d26e
Omar Baldi <omar.baldi.business@gmail.com>
2024-04-05 12:50:01 +0200

    Added event listener logic to keep track of the grid cell pressed and highlight relevant grid cell using background-color

--- src/components/SelectableGrid.tsx
+++ src/components/SelectableGrid.tsx
@@ -1,4 +1,5 @@
 /* eslint-disable */
+import { useEffect, useRef, useState } from "react";
 
 const defaultTotalAmountElements = 150;
 const defaultElementsPerRow = 15;
@@ -12,12 +13,49 @@ export default function SelectableGrid({
   totalAmountElements = defaultTotalAmountElements,
   elementsPerRow = defaultElementsPerRow,
 }: Props) {
+  const gridRef = useRef<HTMLDivElement>(null);
+  const isPressing = useRef<boolean>(false);
+  const firstGridCellPressed = useRef<number | null>(null);
+  const [selectedGridCells, setSelectedGridCells] = useState<number[]>([]);
+
+  useEffect(() => {
+    function handleMouseDownEvent(e: MouseEvent) {
+      isPressing.current = true;
+
+      if (e.target instanceof HTMLElement) {
+        const gridCell = e.target.innerText;
+        const isGridCellValid = !isNaN(+gridCell);
+
+        if (isGridCellValid) {
+          firstGridCellPressed.current = +gridCell;
+          setSelectedGridCells([firstGridCellPressed.current]);
+        }
+      }
+    }
+
+    function handleMouseUpEvent() {
+      isPressing.current = false;
+      setSelectedGridCells([]);
+    }
+
+    gridRef.current?.addEventListener("mousedown", handleMouseDownEvent);
+    gridRef.current?.addEventListener("mouseup", handleMouseUpEvent);
+
+    return function () {
+      gridRef.current?.removeEventListener("mousedown", handleMouseDownEvent);
+      gridRef.current?.removeEventListener("mouseup", handleMouseUpEvent);
+    };
+  }, []);
+
   const gridCells = [...Array(totalAmountElements)]
     .map((_, i) => i + 1)
     .map((n) => {
+      const isSelected = selectedGridCells.includes(n);
+
       return (
         <div
           key={n}
+          data-grid-cell={n}
           style={{
             height: "50px",
             width: "50px",
@@ -25,6 +63,7 @@ export default function SelectableGrid({
             display: "flex",
             justifyContent: "center",
             alignItems: "center",
+            backgroundColor: isSelected ? "lightblue" : "initial",
           }}
         >
           <span style={{ fontWeight: "bold" }}>{n}</span>
@@ -34,6 +73,7 @@ export default function SelectableGrid({
 
   return (
     <div
+      ref={gridRef}
       style={{ display: "grid", gridTemplateColumns: `repeat(${elementsPerRow},1fr)` }}
     >
       {gridCells}
