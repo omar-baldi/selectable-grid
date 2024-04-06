44decc0585e85c4963106e6a5c64dc0b542f17b7
Omar Baldi <omar.baldi.business@gmail.com>
2024-04-06 21:03:06 +0200

    Added test to assert correct range of grid cells is selected

--- tests/unit/components/SelectableGrid.test.tsx
+++ tests/unit/components/SelectableGrid.test.tsx
@@ -33,4 +33,29 @@ describe("SelectableGrid", () => {
     fireEvent.mouseEnter(gridCellElement);
     expect(gridCellElement).toHaveStyle({ backgroundColor: "initial" });
   });
+
+  it("should select grid cells when dragging from a previously pressed cell to another", () => {
+    const wrapper = render(<SelectableGrid />);
+    const startGridCellElement = wrapper.getByTestId("grid-cell grid-cell-49", {
+      exact: true,
+    });
+
+    const endGridCellElement = wrapper.getByTestId("grid-cell grid-cell-82", {
+      exact: true,
+    });
+
+    fireEvent.mouseDown(startGridCellElement);
+    fireEvent.mouseEnter(endGridCellElement);
+
+    const expectedSelectedGridCells = [49, 50, 51, 52, 64, 65, 66, 67, 79, 80, 81, 82];
+    for (const expectedSelectedGridCell of expectedSelectedGridCells) {
+      const gridCellElement = wrapper.getByTestId(
+        `grid-cell grid-cell-${expectedSelectedGridCell}`,
+        { exact: true }
+      );
+
+      expect(gridCellElement).toBeInTheDocument();
+      expect(gridCellElement).toHaveStyle({ backgroundColor: "lightblue" });
+    }
+  });
 });
