3a483de8ea55230f3a1ad2192ab85d1076ed7c04
Omar Baldi <omar.baldi.business@gmail.com>
2024-04-07 11:00:38 +0200

    Added test to assert correct selected grid cells range

--- tests/unit/helpers/grid-cell.test.ts
+++ tests/unit/helpers/grid-cell.test.ts
@@ -34,4 +34,31 @@ describe("getGridCellsRange", () => {
       });
     }).toThrow(new Error("Indexes need to be a pair"));
   });
+
+  it.each([
+    [
+      {
+        first: [2, 9],
+        last: [4, 9],
+        expectedGridCellsRange: [40, 55, 70],
+      },
+    ],
+    [
+      {
+        first: [1, 3],
+        last: [3, 6],
+        expectedGridCellsRange: [19, 20, 21, 22, 34, 35, 36, 37, 49, 50, 51, 52],
+      },
+    ],
+  ])(
+    "should return correct grid cells range",
+    ({ first, last, expectedGridCellsRange }) => {
+      const gridCellsRange = getGridCellsRange({
+        cellIndexes: { first, last },
+        elementsPerRow: 15,
+      });
+
+      expect(gridCellsRange).toEqual(expectedGridCellsRange);
+    }
+  );
 });
